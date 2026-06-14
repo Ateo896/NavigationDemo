0d18ee005edce7a5e68df407b4647664b9fef278
Ateo <tereshkov896@gmail.com>
2026-06-15 03:47:47 +0700

    add Home.kt

---
+++ app/src/main/java/com/example/navigationdemo/screens/Home.kt
@@ -0,0 +1,50 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigationdemo.WelcomeScreen
+
+@Composable
+fun Home(onNavigation: (NavKey) -> Unit) {
+    var userName by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            CustomTextField(
+                title = "Enter your name",
+                textState = userName,
+                onTextChange = { userName = it }
+            )
+            Spacer(modifier = Modifier.size(30.dp))
+            Button(onClick = { onNavigation(WelcomeScreen(userName)) }) {
+                Text("Register")
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomTextField(
+    title: String,
+    textState: String,
+    onTextChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = textState,
+        onValueChange = onTextChange,
+        singleLine = true,
+        label = { Text(title) },
+        modifier = Modifier.padding(10.dp),
+        textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp)
+    )
+}
